2d877a012448aae835a28d78edfe878bd3540a32
James Orr <orrjamesw@gmail.com>
2015-01-31 14:00:29 -0500

    oh god power outage

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskell, ScopedTypeVariables #-}
 
 module Main where
 
@@ -36,12 +36,19 @@ data BreathState = BreathState { _level :: Float
 
 data GameState = GameState { _keyState :: KeyboardState
                            , _breath :: BreathState
-                           , _viewPort :: ViewPort
+                           , _camera :: Camera
                            , _bird :: Bird
                            , _assets :: Assets
                            , _time :: Float
                            , _ripplePos :: Ripple
+                           , _gen :: StdGen
                            }
+
+data Camera = Camera { _viewPort :: ViewPort
+                     , _targetPos :: (Float, Float)
+                     , _cameraSpeed :: Float
+                     , _followBird :: Bool
+                     }
 
 data KeyboardState = KeyboardState { _spacePressed :: Bool
                                    , _lastPress :: Float
@@ -62,6 +69,7 @@ data BirdEvent = Curve { _duration :: Float
 data Bird = Bird { _position :: (Float, Float)
                  , _rotation :: Float
                  , _event :: BirdEvent
+                 , _flapDuration :: Float
                  }
 
 data Assets = Assets { _birdDown :: Picture
@@ -77,16 +85,47 @@ makeLenses ''KeyboardState
 makeLenses ''Bird
 makeLenses ''Assets
 makeLenses ''BirdEvent
-makePrisms ''BirdEvent            
+makePrisms ''BirdEvent     
+makeLenses ''Camera
 
-initialState :: Assets -> GameState
-initialState assets = GameState (KeyboardState False 1000000) (BreathState 30 Exhale 0 0) viewPortInit initialBird assets 0 (-200,-100)
+initialState :: StdGen -> Assets -> GameState
+initialState gen assets = GameState (KeyboardState False 1000000) (BreathState 30 Exhale 0 0) initialCamera initialBird assets 0 (-200,-100) gen
+
+initialCamera :: Camera
+initialCamera = Camera viewPortInit (0,0) 10 False
 
 initialBird :: Bird
---initialBird = Bird (-400,100) 0 (Straight 10 50)
-initialBird = Bird (-400, 100) 0 (Curve 1 0.1 1 0 1 200 1)
+initialBird = Bird (-500,100) 0 (Straight 4 100) (-1)
+-- initialBird = Bird (-400, 100) 0 (Curve 1 0.1 1 0 1 200 1)
 
 type Time = Float
+
+randomizeBirdEvent :: State GameState ()
+randomizeBirdEvent = do
+    g <- fmap _gen $ get
+    game <- get
+    let (val :: Float, g') = randomR (0,2) g
+    case game^.bird.event of
+      Straight _ _ -> newCurve g
+      Curve _ _ _ _ _ _ _ -> newStraight g
+    bird.flapDuration .= 0.2
+    camera.followBird .= True
+  where
+    newStraight g = let (d, g'') = randomR (2, 5) g
+                        (s, g''') = randomR (50, 200) g''
+                     in do
+                        bird.event .= Straight d s
+                        gen .= g'''
+    newCurve g = let (d, g2) = randomR (1, 3) g
+                     (cl, g3) = randomR (0,2) g2
+                     (cc, g4) = randomR (1,3) g3
+                     (ga, g5) = randomR (-20,200) g4
+                     (angs, g6) = randomR (0.5, 3) g5
+                     (airs, g7) = randomR (50,200) g6
+                     dr = cc
+                  in do
+                     bird.event .= Curve d cl cc ga angs airs dr
+                     gen .= g7
 
 tick :: Float -> GameState -> IO GameState
 tick f game = return $ flip execState game $ do
@@ -107,13 +146,22 @@ tick f game = return $ flip execState game $ do
         bird.rotation += (dr + lin * f) * angspd
         bird.position._1 += f * airspd * cos (gameState^.bird.rotation / 180 * pi)
         bird.position._2 += f * airspd * sin (gameState^.bird.rotation / 180 * pi)
-        bird.event %= (duration -~ f)
+        e <- bird.event <%= (duration -~ f)
+        when (e^.duration < 0 && abs (gameState^.bird.rotation - (_goalAngle e)) < 100) randomizeBirdEvent
+        when (gameState^.bird.rotation > 360) (bird.rotation -= 360)
+        when (gameState^.bird.rotation < 0) (bird.rotation += 360)
       Straight d s -> do 
         bird.position._1 += f * s * cos (gameState^.bird.rotation / 180 * pi)
         bird.position._2 += f * s * sin (gameState^.bird.rotation / 180 * pi)
-        bird.event %= (duration -~ f)
+        e <- bird.event <%= (duration -~ f)
+        when (e^.duration < 0) randomizeBirdEvent
     time += f
-    viewPort.viewTranslate .= ((gameState^.bird.position) & each *~ (-1))
+    when (gameState^.camera.followBird) $
+         do
+           let dx = gameState^.bird.position._1 + camera.viewPort.viewTranslate._1
+               dy = gameState^.bird.position._2 + camera.viewPort.viewTranslate._2
+           (camera.viewPort.viewTranslate .= ((gameState^.bird.position) & each *~ (-1)))
+    bird.flapDuration -= f
 
 viewTranslate :: Lens' ViewPort (Float, Float)
 viewTranslate f (ViewPort t r s) = fmap (\t' -> ViewPort t' r s) (f t)
@@ -134,7 +182,7 @@ processInput (EventKey (SpecialKey KeySpace) Down _ _) game = return . flip exec
     if (inExhaleRing (game ^. breath) && game ^. breath.direction == Exhale) 
       then do breath.currentStreak += 0
               breath.direction .= Inhale
-      else when (not (inInhaleRing (game ^.breath) || fastPress game)) (breath.currentStreak .= 0)
+      else when (not (inInhaleRing (game ^.breath) || fastPress game)) missBreath
     keyState.spacePressed .= True
     keyState.lastPress .= 0
     breath.longestStreak %= max (game^.breath.currentStreak)
@@ -145,7 +193,7 @@ processInput (EventKey (SpecialKey KeySpace) Up _ _) game = return . flip execSt
     if inInhaleRing b && b^.direction == Inhale
        then do breath.currentStreak += 1
                breath.direction .= Exhale
-       else when (not (inExhaleRing b || fastPress game)) (breath.currentStreak .= 0)
+       else when (not (inExhaleRing b || fastPress game)) missBreath
     keyState.spacePressed .= False
     keyState.lastPress .= 0
     breath.longestStreak %= max (game^.breath.currentStreak)
@@ -154,6 +202,13 @@ processInput _ game = return game
 between :: Ord a => a -> a -> a -> Bool
 between target low hi = low < target && target < hi
 
+missBreath :: State GameState ()
+missBreath = do game <- get
+                breath.currentStreak .= 0
+                camera.viewPort.viewTranslate .= (0,0)
+                when (game^.camera.followBird == True) (bird .= initialBird)
+                camera.followBird .= False
+
 inInhaleRing :: BreathState -> Bool
 inInhaleRing breath = between (breath ^. level) (inhaleTo - inhaleDiameter/2) (inhaleTo + inhaleDiameter/2)
 
@@ -161,7 +216,7 @@ inExhaleRing :: BreathState -> Bool
 inExhaleRing breath = between (breath ^. level) (exhaleTo - exhaleDiameter/2) (exhaleTo + exhaleDiameter/2)
 
 renderScene :: GameState -> Picture
-renderScene game = applyViewPortToPicture (game^.viewPort) $ 
+renderScene game = applyViewPortToPicture (game^.camera.viewPort) $ 
                    mconcat  [ renderRiver game
                             , renderBreath game
                             , renderBird game
@@ -205,7 +260,11 @@ renderExhaleRing gameState = if gameState^.breath.direction == Exhale then if in
 renderBird :: GameState -> Picture
 renderBird game = uncurry translate (game^.bird.position) $ 
                   rotate ((-1) * game^.bird.rotation) $
-                  game^.assets.birdUp
+                  birdPic
+  where
+    birdPic = if game^.bird.flapDuration > 0
+                then game^.assets.birdDown
+                else game^.assets.birdUp
 
 renderRipple :: GameState -> Picture
 renderRipple game = translate x y $ game^.assets.ripple
@@ -218,4 +277,4 @@ main = do
   Just ripple <- loadJuicyPNG "ripple.png"
   g <- getStdGen
   let assets = Assets  (scale 0.2 0.2 $ birdDown) (scale 0.2 0.2 $ birdUp) (scale 0.2 0.2 $ ripple)
-  playIO (InWindow "Relax!" (800, 600) (0,0)) background 60 (initialState assets) render processInput tick
+  playIO (InWindow "Breathe :)" (800, 600) (0,0)) background 60 (initialState g assets) render processInput tick
